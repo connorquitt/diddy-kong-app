0a15bf5854dd16466d8f90a409f8df971da15553
connorquitt <133990316+connorquitt@users.noreply.github.com>
2024-11-17 11:34:38 -0600

    Update seed.py
    added all characters

--- server/seed.py
+++ server/seed.py
@@ -1,2 +1,172 @@
 import datetime
 from app import app
+
+Character/method	Pivot down toss, front of shield	Pivot down toss, back of shield	SH down toss, front of shield	SH down toss, back of shield	RAR down toss, front of shield	RAR down toss, back of shield	Z drop oos, front 	Z drop oos, back of shield
+
+Mario	No	Yes	Yes 	No	Yes	Yes	Yes	Yes
+
+Donkey Kong	No	Yes	No	Yes	No 	No	No	No
+
+Link	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Samus	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+
+Yoshi	No	Yes	No	No	No 	No	Yes	Yes (spaced)
+
+Kirby	Yes	Yes	No	No	No 	No	No	Yes (spaced)
+
+Fox	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Pikachu	No	No	Yes 	No	Yes	No	Yes	Yes (spaced)
+
+Luigi	No	Yes	Yes 	No	Yes	Yes	No	Yes
+
+Ness	No	Yes	No	Yes	Yes	Yes	No	Yes
+
+Captain Falcon	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Jigglypuff	Yes	Yes	No	No	Yes	Yes	Yes	Yes (spaced)
+
+Paisy	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Bowser	No	Yes	No	Yes	No 	No	No	No
+
+Ice Climbers	Yes	Yes	Yes 	No	Yes	Yes	Yes (spaced)	Yes
+
+Sheik	Yes	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Zelda	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Dr. Mario	No	Yes	Yes 	No	Yes	Yes	Yes	Yes
+
+Pichu	No	Yes	No	No	No 	No	No	No
+
+Falco	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Marth	Yes	Yes	Yes 	Yes	Yes	Yes	Yes (spaced)	Yes (spaced)
+
+Lucina	Yes	Yes	Yes 	Yes	Yes	No	No	Yes (spaced)
+
+Young Link	No	Yes	No	No	Yes	Yes	No	Yes
+
+Ganondorf	No	Yes	No	Yes	Yes	Yes	No	No
+
+Mewtwo	No	Yes	Yes 	Yes	Yes	No	No	No
+
+Roy	No	Yes	Yes 	Yes	Yes	No	No	Yes (spaced)
+
+Chrom	No	Yes	Yes 	Yes	Yes	No	No	Yes (spaced)
+
+Mr. Game & Watch	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Meta knight	No	Yes	Yes 	No	Yes	Yes	Yes	Yes
+
+Pit	Yes	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+
+Zero Suit Samus	Yes	Yes	Yes 	Yes	Yes	No	No	No
+
+Wario	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Snake	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Ike	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Squirtle	No	Yes	No	No	No 	No	No	No
+
+Ivysaur	Yes	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Charizard	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Diddy Kong	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Lucas	No	Yes	No	No	Yes	No	No	Yes
+
+Sonic	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+King Dedede	No	No	Yes 	No	Yes	No	No	No
+
+Olimar	No	No	No	No	Yes	No	Yes	Yes
+
+Lucario	No	No	No	No	Yes	Yes	Yes	Yes
+
+ROB	No	No	Yes 	No	Yes	Yes	No	Yes (spaced)
+
+Toon Link	No	Yes	No	No	Yes	Yes	Yes	Yes
+
+Wolf	Yes	No	Yes 	No	Yes	Yes	Yes	Yes
+
+Villager	No	No	No	No	Yes	Yes	Yes	Yes
+
+Mega Man	Yes	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Wii Fit Trainer	Yes	Yes	Yes 	Yes	Yes	Yes	No	No
+
+Rosalina & Luma	No	Yes	No	No	No 	No	No	No
+
+Little Mac	No	Yes	Yes 	Yes	Yes	Yes	Yes	No
+
+Greninja	No	Yes	Yes 	No	Yes	Yes	Yes	Yes (spaced)
+
+Palutena	No	Yes	No	No	No 	No	No	No
+
+Pac Man	No	Yes	No	No	Yes	Yes	No	Yes
+
+Robin	No	Yes	Yes 	Yes	No 	No	No	Yes (spaced)
+
+Shulk	Yes	Yes	Yes 	Yes	Yes	Yes	Yes	Yes (spaced)
+
+Bowser Jr.	No	No	Yes 	No	Yes	Yes	No	No
+
+Duck Hunt Duo	No	Yes	No	No	No 	Yes	No	No
+
+Ryu	Yes	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Ken	Yes	Yes	Yes 	Yes	Yes	Yes	Yes (spaced)	Yes
+
+Cloud	Yes	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Corrin	No	Yes	Yes 	Yes	Yes	Yes	Yes	Yes
+
+Bayonetta	Yes	No	Yes 	No	No 	No	No	No
+
+Inkling	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Ridley	No	Yes	Yes 	No	No 	No	No	No
+
+Simon	Yes	Yes	Yes 	Yes	Yes	Yes	Yes	Yes 
+
+King. K. Rool	Yes	Yes	Yes 	Yes	No 	No	No	No
+
+Isabelle	No	No	No	No	Yes	No	No	Yes
+
+Incineroar	No	Yes	Yes 	Yes	Yes	Yes	Yes	No
+
+Piranha Plant	No	No	Yes 	No	Yes	Yes	No	Yes
+
+Joker	Yes	No	Yes 	No	Yes	No	No	No
+
+Hero	Yes	No	Yes 	No	Yes	Yes	Yes	Yes
+
+Banjo & Kazooie	Yes	Yes	Yes	Yes	Yes	Yes	Yes	Yes
+
+Terry	Yes	Yes	Yes	Yes	Yes	Yes	Yes	Yes
+
+Byleth	Yes	No	Yes	No	Yes	Yes	Yes	Yes
+
+Min Min	No	No	No	No	No 	No	No	No
+
+Steve	No	No	No	Yes	Yes	Yes	No	Yes
+
+Sephiroth	No	No	No	No	No 	No	No	No
+
+Aegis	Yes	Yes	Yes	Yes	Yes	Yes	Yes	Yes
+
+Kazuya	No	Yes	Yes	Yes	No 	No	No	Yes
+
+Mii Brawler	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Mii Swordfighter	No	Yes	Yes 	Yes	Yes	Yes	No	Yes
+
+Mii Gunner	No	Yes	Yes	No	Yes	Yes	No	Yes
